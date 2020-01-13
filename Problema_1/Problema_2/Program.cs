775f927c68b141fc3f3e57fd546efe84f7d8ff27
vladtf <vendeta0263@gmail.com>
2020-01-13 21:24:35 +0200

    Added second solved problem Reading and displaying propreties of an object.

---
+++ Problema_1/Problema_2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problema_2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Pereche pereche = new Pereche();
+
+            string s = "";
+
+            while (s!="n")
+            {
+                Console.Write("x = ");
+                s = Console.ReadLine();
+
+                try
+                {
+                    if (s != "n")
+                    {
+                        pereche.x = int.Parse(s);
+
+                        Console.Write("y = ");
+                        s = Console.ReadLine();
+
+                        pereche.y = int.Parse(s);
+
+                        pereche.Afisare();
+                    }
+                }
+                catch (Exception e)
+                {
+
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+    }
+
+    public class Pereche
+    {
+        public int x;
+        public int y;
+
+        public int Suma()
+        {
+            return x + y;
+        }
+
+        public void Afisare()
+        {
+            Console.WriteLine("Suma x si y este = " + Suma());
+        }
+    }
+}

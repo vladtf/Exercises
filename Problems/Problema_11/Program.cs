b5c288f1bdf72a90c741c970844a9c1bf210e204
vladtf <vendeta0263@gmail.com>
2020-01-24 20:18:21 +0200

    Added 2 more problems ( anagram - csacademt )

---
+++ Problems/Problema_11/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Problema_11
+{
+    internal class Program
+    {
+
+        private static void Main(string[] args)
+        {
+            int n = Int32.Parse(Console.ReadLine());
+
+            int[] Lsize = new int[105];
+            int[] Rsize = new int[105];
+
+            for (int i = 1; i <= n; i++)
+            {
+                string line = Console.ReadLine();
+                string[] data = line.Split(' ');
+
+                var size = Int32.Parse(data[0]);
+
+                if (data[1] == "L")
+                {
+                    Lsize[size]++;
+                }
+                else
+                {
+                    Rsize[size]++;
+                }
+            }
+
+            int rez = 0;
+
+            for (int i = 0; i <= 100; i++)
+            {
+                rez += Math.Min(Rsize[i], Lsize[i]);
+            }
+
+            Console.WriteLine(rez.ToString());
+        }
+        //    private static void Main(string[] args)
+        //    {
+        //        int n = Int32.Parse(Console.ReadLine());
+
+        //        List<Shoe> shoes = new List<Shoe>();
+
+        //        int count = 0;
+
+        //        for (int i = 0; i < n; i++)
+        //        {
+        //            Shoe shoe = new Shoe();
+
+        //            string line = Console.ReadLine();
+
+        //            string[] data = line.Split(' ');
+
+        //            shoe.Size = Int32.Parse(data[0]);
+        //            shoe.Leg = data[1];
+
+        //            if (i > 0)
+        //            {
+        //                foreach (var item in shoes)
+        //                {
+        //                    if (shoe.Leg != item.Leg && shoe.Size == item.Size && !item.IsPaired && !shoe.IsPaired)
+        //                    {
+        //                        count++;
+        //                        item.IsPaired = shoe.IsPaired = true;
+        //                    }
+        //                }
+        //            }
+
+        //            shoes.Add(shoe);
+        //        }
+
+        //        Console.WriteLine(count.ToString());
+
+        //        Console.ReadLine();
+        //    }
+        //}
+
+        //internal class Shoe
+        //{
+        //    public int Size { get; set; }
+        //    public string Leg { get; set; }
+        //    public bool IsPaired { get; set; } = false;
+        //}
+    }
+}
---
+++ Problems/Problema_12/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problema_12
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int n = Int32.Parse(Console.ReadLine());
+
+            List<string> words = new List<string>();
+
+            List<Anagram> anagrams = new List<Anagram>();
+
+            for (int i = 0; i < n; i++)
+            {
+                char[] word = Console.ReadLine().ToCharArray();
+
+                Array.Sort(word);
+
+                words.Add(new string(word));
+            }
+
+            anagrams.Add(new Anagram { Word = words.First(), Count = 0 });
+
+            int max = 0;
+
+            foreach (var word in words)
+            {
+                bool ok = false;
+                foreach (var item in anagrams)
+                {
+                    if (String.Compare(word, item.Word) == 0)
+                    {
+                        item.Count++;
+
+                        if (item.Count > max) max = item.Count;
+
+                        ok = true;
+                        break;
+                    }
+                }
+                if (!ok) anagrams.Add(new Anagram { Word = word, Count = 1 });
+            }
+
+            Console.WriteLine(max.ToString());
+
+            Console.ReadLine();
+        }
+    }
+
+    internal class Anagram
+    {
+        public int Count { get; set; } = 1;
+        public string Word { get; set; }
+    }
+}

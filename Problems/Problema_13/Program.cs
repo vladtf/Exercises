966ac79b5cd6d56a3dfbead1833f97a1db76857c
vladtf <vendeta0263@gmail.com>
2020-01-25 19:17:19 +0200

    Added 2 new problems ( used a stringbuilder for a faster concatenatig strings )

---
+++ Problems/Problema_13/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problema_13
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int n = Int32.Parse(Console.ReadLine());
+
+            List<Word> words = new List<Word>();
+
+            for (int i = 1; i <= n; i++)
+            {
+                string word = Console.ReadLine();
+
+                words.Add(new Word { WordStr = word, Ord = i });
+            }
+
+            List<Word> orderedList = words.OrderBy(o => o.WordStr).ToList();
+
+            foreach (var word in orderedList)
+            {
+                Console.Write(word.Ord + " ");
+            }
+        }
+    }
+
+    internal class Word
+    {
+        public string WordStr { get; set; }
+        public int Ord { get; set; }
+    }
+}
---
+++ Problems/Problema_14/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+namespace Problema_14
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int n = Int32.Parse(Console.ReadLine());
+
+            StringBuilder result = new StringBuilder();
+
+            for (int i = 0; i < n; i++)
+            {
+                var str = Console.ReadLine().ToCharArray();
+
+                Array.Sort(str);
+
+                result.Append(str[0]);
+            }
+
+            var ans = result.ToString().ToCharArray();
+
+            Array.Sort(ans);
+
+            Console.WriteLine(new String(ans));
+        }
+    }
+}

4c2de4ba118de5929ed3ad666d87dc107ff5e482
vladtf <vendeta0263@gmail.com>
2020-01-29 22:14:11 +0200

    Added 2 more tasks

---
+++ Problems/Problema_23/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace Problema_23
+{
+    class Program
+    {
+//        Erase Value
+//Time limit: 1220 ms
+//Memory limit: 130.5 MB
+
+//You are given an array AA of NN positive integers.You can choose any positive value xx and erase all the elements of AA equal to xx.In the end you want the sum of the elements of the array to be as small as possible.
+        static void Main(string[] args)
+        {
+            int n = Int32.Parse(Console.ReadLine());
+
+            int[] tokens = Console.ReadLine().Split(' ').Select(Int32.Parse).ToArray();
+
+            int[] freq = new int[n+1];
+
+            int sum = 0;
+
+            foreach (var item in tokens)
+            {
+                sum += item;
+                freq[item]++;
+            }
+
+            int maxValue = 0;
+
+            for (int i = 0; i <= n; i++)
+            {
+                if(freq[i]*i>maxValue)
+                {
+                    maxValue = freq[i] * i;
+                }
+            }
+
+            Console.WriteLine(sum-maxValue);
+        }
+    }
+}
---
+++ Problems/Problema_24/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace Problema_24
+{
+    /*
+     * Min Pairing
+Time limit: 1220 ms
+Memory limit: 130.5 MB
+
+You have an array AA of NN integers, NN is always even. You should group the numbers in N/2N/2 pairs. For each pair you calculate the absolute difference between the two elements. You want to minimize the sum of all the absolute differences.
+     */
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int n = Int32.Parse(Console.ReadLine());
+
+            int[] tokens = Console.ReadLine().Split(' ').Select(x => Int32.Parse(x)).OrderByDescending(x => x).ToArray();
+
+            int dif = 0;
+            for (int i = 0; i < n; i += 2)
+            {
+                dif += tokens[i] - tokens[i + 1];
+            }
+
+            Console.WriteLine(dif);
+        }
+    }
+}

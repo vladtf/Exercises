7608a8ee6bd0b0367cd8f24c9898b2f2a579124a
vladtf <vendeta0263@gmail.com>
2020-01-31 09:08:59 +0200

    Added 2 more tasks ( arrays )

---
+++ Problems/Problema_26/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Problema_26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] str = Console.ReadLine().ToCharArray();
+            char[] ciphr = Console.ReadLine().ToCharArray();
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                str[i] = ciphr[str[i] - 'a'];
+            }
+
+            Console.WriteLine(new string(str));
+        }
+    }
+}
---
+++ Problems/Problema_27/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace Problema_27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tokens = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
+            tokens[1] += Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray().Sum();
+            tokens[2] += Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray().Sum();
+
+            if (tokens[1] > tokens[2])
+            {
+                Console.WriteLine(1);
+            }
+            else if (tokens[1] == tokens[2])
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                Console.WriteLine(2);
+            }
+
+        }
+
+    }
+}

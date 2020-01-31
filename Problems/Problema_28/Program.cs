4baf27d1ecd81bcf03b03702047a69b73deb3d9a
vladtf <vendeta0263@gmail.com>
2020-01-31 20:34:28 +0200

    Added 2 more solved tasks Arrays + strings

---
+++ Problems/Problema_28/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Problema_28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //creaza obiectul nou
+            Numar obiect = new Numar();
+
+            string str = "";
+
+            //Citeste obiecte pana cand nu este introdus caracaterul n
+            while (str != "n")
+            {
+                //Citeste obiectul
+                obiect.Citire();
+
+                //afiseaza x la puterea y din obiect
+                obiect.Afisare();
+
+                //citeste caracterul, dacca este "n" atunci va iesi din for
+                Console.Write("Scrie n pentru a iesi : ");
+                str = Console.ReadLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+    class Numar
+    {
+        public int x;
+        public int y;
+
+
+        //citeste obiectul
+        public void Citire()
+        {
+            Console.Write("Citeste x : ");
+            x = int.Parse(Console.ReadLine()); //transform caracterul citit in intreg
+            Console.Write("Citeste y : ");
+            y = int.Parse(Console.ReadLine()); //transform caracterul citit in intreg
+        }
+
+        //Afiseaza x la puterea y
+        public void Afisare()
+        {
+            Console.WriteLine("X la puterea Y este : " + xPutereY().ToString());
+        }
+
+        public int xPutereY()
+        {
+            // x la puterea 0 este 1
+            int result = 1;
+
+            // inumulteste rezultatul cu x de y ori -> x*x*x*x*...*x = x la puterea y
+            for (int i = 1; i <= y; i++)
+            {
+                result = result * x;
+            }
+
+            //returneaza x la puterea y
+            return result;
+        }
+
+    }
+
+}
---
+++ Problems/Problema_29/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Problema_29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<CustomObject> lista = new List<CustomObject>();
+            Console.Write("Citeste sir de caractere ('n' pentru a iesi) :");
+            string str = Console.ReadLine();
+            while (str.Length >= 5)
+            {
+                CustomObject obiect = new CustomObject();
+                obiect.PrelucrareSir(str);
+                Console.WriteLine();
+                Console.Write("Citeste al doilea sir de caracter ('n' pentru a iesi) : ");
+                obiect.Citire();
+                if (obiect.sir1 == "n")
+                {
+                    lista.Add(obiect);
+                    break;
+                }
+
+                lista.Add(obiect);
+
+                Console.Write("Citeste sir de caractere ('n' pentru a iesi) :");
+                str = Console.ReadLine();
+                if (str == "n")
+                    break;
+            }
+
+            foreach (var item in lista)
+            {
+                Console.WriteLine("Sirul initial este : " + item.sirInitial);
+                Console.WriteLine("Sirul prelucrate este : " + item.str);
+                Console.Write("Structura de date din sir :");
+                foreach (var numar in item.Numbers)
+                {
+                    Console.Write(numar);
+                    Console.Write(" , ");
+                }
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    public class CustomObject
+    {
+        public string str = "";
+        public string sirInitial = "";
+
+        public List<int> Numbers = new List<int>();
+
+        public void PrelucrareSir(string s)
+        {
+            sirInitial = s;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if ((s[i] >= 'a' && s[i] <= 'z') || s[i] == ' ')
+                    str += s[i];
+                else
+                {
+                    string x = "";
+                    while (s[i] >= '1' && s[i] <= '9')
+                    {
+                        x += s[i];
+                        i++;
+                        if (i >= s.Length)
+                            break;
+                        if (!(s[i] >= '1' && s[i] <= '9'))
+                            break;
+                    }
+                    Numbers.Add(int.Parse(x));
+                    i--;
+                }
+            }
+        }
+
+        public string sir1 = "";
+        public int[] frecventa = new int[32];
+        public void Citire()
+        {
+            sir1 = Console.ReadLine();
+
+            for (int i = 0; i < sir1.Length; i++)
+            {
+                frecventa[(int)(sir1[i] - 'a')]++;
+            }
+        }
+
+    }
+
+}

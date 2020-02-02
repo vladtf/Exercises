cfee45813c83f59baaea3836b312247fabaf2e60
vladtf <vendeta0263@gmail.com>
2020-02-02 19:27:40 +0200

    Added 2 more task ( 2 line solve with lambda exxpresion and length task )

---
+++ Problems/Problema_30/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Problema_30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = Console.ReadLine();
+
+            int distX = 0;
+            int distY = 0;
+            for (int i = 0; i < line.Length; i++)
+            {
+                switch (line[i])
+                {
+                    case 'N': 
+                        distY++;
+                        break;
+                    case 'S':
+                        distY--;
+                        break;
+                    case 'E':
+                        distX++;
+                        break;
+                    case 'W':
+                        distX--;
+                        break;
+                }
+            }
+            Console.WriteLine(Math.Abs(distX)+Math.Abs(distY));
+        }
+    }
+}
---
+++ Problems/Problema_31/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace Problema_31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = Int32.Parse(Console.ReadLine())-101;
+            Console.WriteLine(Console.ReadLine().Split(' ').Select(x => Int32.Parse(x)).Where(x => { if (x > n) { n = x; return true; } return false; }).Count());
+
+            //int count = 0;
+            //Console.ReadLine().Split(' ').Select(x => Int32.Parse(x)).ToList().ForEach(x => { if (x > n) { n = x; count++; } });
+            //Console.WriteLine(count);
+        }
+    }
+}

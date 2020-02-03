b9f260820ebf7d0f4066c33b51e4025a13895687
vladtf <vendeta0263@gmail.com>
2020-02-03 23:30:37 +0200

    Added 2 more tasks ( lists and frequency )

---
+++ Problems/Problema_32/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Problema_32
+{
+    class Program
+    {
+        //        Four X-tremes
+        //   Time limit: 1220 ms
+        //   Memory limit: 130.5 MB
+
+        //   Template
+        //You should implement a function that takes as arguments four integers.Return the maximum difference between any two integers.
+        static void Main(string[] args)
+        {
+            var line = Console.ReadLine().Split(' ').Select(x=>Int32.Parse(x)).OrderBy(x=>x).ToList();
+            Console.WriteLine(Math.Abs(line.First()-line.Last()));
+        }
+    }
+}
---
+++ Problems/Problema_33/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace Problema_33
+{
+    internal class Program
+    {
+
+        //    Expected Dice
+        //Time limit: 1220 ms
+        //Memory limit: 130.5 MB
+
+        //You have two special dice with 66 faces, each.For each dice you know the numbers written on each face.
+
+        //You roll the two dices at the same time, and you add up the numbers showing on the upper faces.What is the most probable sum value you'll get?
+        private static void Main(string[] args)
+        {
+            //var x = Console.ReadLine().Split(' ').Select(x => Int32.Parse(x)).GroupBy(x => x).OrderByDescending(group => group.Count()).First().First();
+            //var y = Console.ReadLine().Split(' ').Select(x => Int32.Parse(x)).GroupBy(x => x).OrderByDescending(group => group.Count()).First().First();
+
+            int[] freq = new int[101];
+
+            var First = Console.ReadLine().Split(' ').Select(x => Int32.Parse(x)).ToList();
+            var Second = Console.ReadLine().Split(' ').Select(x => Int32.Parse(x)).ToList();
+
+            foreach (var item in First)
+            {
+                foreach (var item2 in Second)
+                {
+                    freq[item + item2]++;
+                }
+            }
+
+            Console.WriteLine(freq.ToList().IndexOf(freq.ToList().OrderByDescending(x => x).First()));
+        }
+
+    }
+}

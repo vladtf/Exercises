039825fa020d507b1d5ccd56f5113eb29a4e1cb0
vladtf <vendeta0263@gmail.com>
2020-02-05 23:59:26 +0200

    Solved 2 more tasks  ( lists and string arrays )

---
+++ Problems/Problema_36/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace Problema_36
+{
+    class Program
+    {
+        /*
+         * Xor Match
+         * Time limit: 1220 ms
+         * Memory limit: 258.5 MB
+         * You are given two binary arrays, AA of size NN and BB of size MM, N \leq MN≤M. We consider the arrays to be 0-indexed.
+         * A xor-match is an occurrence of the binary complement of AA in BB. Formally, it is an index jj, 0 \leq j \leq M - N0≤j≤M−N, such that A_i + B_{i+j} = 1A
+         * i​​ +B​i+j​​ =1, for all 0 \leq i < N0≤i<N.
+         * Find the number of xor-matches.
+         */
+        static void Main(string[] args)
+        {
+            int[] tokens = Console.ReadLine().Split(' ').Select(x => Int32.Parse(x)).ToArray();
+            int n = tokens[0];
+            int m = tokens[1];
+
+            int[] A = Console.ReadLine().Split(' ').Select(x => Int32.Parse(x)).ToArray();
+            int[] B = Console.ReadLine().Split(' ').Select(x => Int32.Parse(x)).ToArray();
+
+            int ans = 0;
+            for (int i = 0; i < m-n+1; i++)
+            {
+                bool ok = true;
+                for (int j = 0; j < n && ok; j++)
+                {
+                    if (A[j] + B[i+j] != 1)
+                    {
+                        ok = false;
+                    }
+                }
+                if (ok)
+                {
+                    ans++;
+                }
+            }
+            Console.WriteLine(ans);
+        }
+    }
+}
---
+++ Problems/Problema_37/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+
+namespace Problema_37
+{
+    class Program
+    {
+//        Replace A
+//Time limit: 1220 ms
+//Memory limit: 258.5 MB
+
+//You are given a string SS containing only letters A or B.You can take any two adjacent As and replace them by a single A.You perform operations as long as possible.Print the final string.
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(Console.ReadLine().Split('B').Select(x=>x.First()).Select(x => "AB").Aggregate((current, next) => current + next)); 
+
+            //var line = Console.ReadLine().Select(x => x.ToString()).ToArray();
+            //string ans = line.Aggregate((current, value) =>
+            //{
+            //    if (next != null)
+            //    {
+            //        if (value == "B")
+            //        {
+            //            return value + "";
+            //        }
+            //        if (value == next && value == "A")
+            //        {
+            //            return "";
+            //        }
+            //        return value + next;
+            //    }
+            //    return "";
+            //});
+
+            var line = Console.ReadLine().ToCharArray();
+            string ans = "";
+            for (int i = 0; i < line.Length - 1; i++)
+            {
+                if (line[i] == 'B')
+                {
+                    ans += 'B';
+                }
+                else if (line[i] != line[i + 1] && line[i] == 'A')
+                {
+                    ans += 'A';
+                }
+            }
+            ans += line.Last();
+            Console.WriteLine(ans);
+        }
+    }
+}

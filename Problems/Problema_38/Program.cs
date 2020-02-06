fa79ff5e4ec8e28c802ab8d9aff882adb0f7b369
vladtf <vendeta0263@gmail.com>
2020-02-06 20:09:57 +0200

    Solved 2 tasks ( string and math mean )

---
+++ Problems/Problema_38/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace Problema_38
+{
+    class Program
+    {
+        //You are given a word SS consisting of lowercase English letters.Each letter takes a certain given amount of time to print.How long will it take to print the whole word?
+        static void Main(string[] args)
+        {
+            var lettersCount = Console.ReadLine().ToCharArray().GroupBy(x => x).ToList();
+            var time = Console.ReadLine().Split(' ').Select(x => Int32.Parse(x)).ToList();
+            var ans = lettersCount.Select(x => time[x.First() - 'a'] * x.Count()).Sum();
+            Console.WriteLine(ans);
+        }
+        //class Solution
+        //{
+        //    void Solve()
+        //    {
+        //        string s = ReadLine();
+        //        int[] times = ReadInts();
+        //        int ans = 0;
+        //        foreach (char c in s)
+        //        {
+        //            ans += times[c - 'a'];
+        //        }
+        //        Println(ans);
+        //    }
+
+        //    static void Main(string[] args) { new Solution().Solve(); }
+
+        //    static string ReadLine() { return Console.ReadLine(); }
+        //    static string[] ReadTokens() { return ReadLine().Split(new char[] { ' ' }); }
+        //    static int[] ReadInts() { return ConvertAll(ReadTokens(), int.Parse); }
+
+        //    static void Println(object o) { Console.WriteLine(o); }
+        //    static void Print(object o) { Console.Write(o); }
+        //}
+    }
+}
---
+++ Problems/Problema_39/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace Problema_39
+{
+    class Program
+    {
+//        Grade System
+//Time limit: 1220 ms
+//Memory limit: 258.5 MB
+
+//Considering a set of NN grades, an interesting grading system consists of removing one of the minimum and one of the maximum values, and then finding the arithmetic mean of the rest of the N-2N−2 grades.
+
+//Given a set of NN grades, print the result of the grading system.
+        static void Main(string[] args)
+        {
+            var n = Int32.Parse(Console.ReadLine());
+            var tokens = Console.ReadLine().Split(' ').Select(x=> Int32.Parse(x)).ToList();
+            var max = tokens.Max();
+            var min = tokens.Min();
+            var ans = (int)((tokens.Sum() - min - max) / (n - 2));
+            Console.WriteLine(ans);
+        }
+    }
+}

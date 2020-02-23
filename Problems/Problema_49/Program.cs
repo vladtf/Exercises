d768a66176fc4de17228d53ce195b9305d8a686b
vladtf <vendeta0263@gmail.com>
2020-02-23 22:38:47 +0200

    Added 2 more tasks ( bits and array with bits + greedy )

---
+++ Problems/Problema_49/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problema_49
+{
+    class Program
+    {
+        //        Flip Game
+        //Time limit: 1220 ms
+        //Memory limit: 130.5 MB
+
+        //You are given a binary matrix of NN rows and MM columns.You can perform the following type of operations:
+
+        //Choose a row/column and change the state of each element.If the elment is 00 change it to 11, and if it's 11 change it to 00.
+        //You are allowed to perform an unlimited number of operations. In the end you interpret each row as the binary representation of a number, where the first column is associated with the most significant bit. You should maximize the sum of all the NN numbers.
+        static void Main(string[] args)
+        {
+
+            var line = Console.ReadLine().Split(' ').Select(x => Int32.Parse(x)).ToList();
+            int n = line[0];
+            int m = line[1];
+
+            List<List<int>> matrix = new List<List<int>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var tokens = Console.ReadLine().Trim().Split(' ').Select(x => Int32.Parse(x)).ToList();
+                matrix.Add(tokens);
+
+                if (matrix[i][0] == 0)
+                {
+                    matrix[i] = matrix[i].Select(x => x == 0 ? 1 : 0).ToList();
+                }
+
+            }
+
+            List<int> columnsToFlip = new List<int>();
+
+            for (int i = 1; i < m; i++)
+            {
+                int sum = 0;
+                for (int j = 0; j < n; j++)
+                {
+                    sum += matrix[j][i];
+                }
+
+                if (sum <= n / 2)
+                {
+                    columnsToFlip.Add(i);
+                }
+            }
+
+            long ans = 0;
+            foreach (var row in matrix)
+            {
+                foreach (int item in columnsToFlip)
+                {
+                    row[item] = row[item] == 0 ? 1 : 0;
+                }
+
+                ans += Convert.ToInt64(String.Join("", row.Select(x => x.ToString())), 2);
+            }
+
+            Console.WriteLine(ans);
+
+        }
+    }
+}
---
+++ Problems/Problema_50/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace Problema_50
+{
+    //    Bit Count
+    //Time limit: 1220 ms
+    //Memory limit: 130.5 MB
+
+    //Template
+    //You are given an integer NN that can be represented on 3232 bits.Implement a function that counts the numbers of bits set to 11 in NN's binary representation.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int n = Int32.Parse(Console.ReadLine().Trim());
+
+            //int ans = 0;
+
+            //while (n > 0)
+            //{
+            //    if (n % 2 == 1)
+            //    {
+            //        ans++;
+            //    }
+            //    n /= 2;
+            //}
+
+            //Console.WriteLine(ans);
+
+            Console.WriteLine(Convert.ToString(Int32.Parse(Console.ReadLine().Trim()),2).Where(x=>x=='1').Count());
+        }
+    }
+}

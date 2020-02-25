e32722fde0ed14425ce240a6403943e91df2a5aa
vladtf <vendeta0263@gmail.com>
2020-02-25 23:11:21 +0200

    Added 2 more tasks ( bitwise operation )

---
+++ Tasks ( 51-100)/Task_1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+
+namespace Task_1
+{
+    class Program
+    {
+        //        Bit Removal
+        //Time limit: 1220 ms
+        //Memory limit: 130.5 MB
+
+        //Template
+        //You should implement a function that takes two integer arguments x\ yx y, where both xx and yy are non-negative integers.Return the result of flipping all of the bits of xx that are set to 11 in both xx and yy.
+        static void Main(string[] args)
+        {
+            int n = Int32.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                var tokens = Console.ReadLine().Split(' ').Select(x => uint.Parse(x)).ToList();
+
+                var a = tokens[0];
+                var b = tokens[1];
+
+                var bitsToFlip = a & b;
+
+                var ans = a ^ bitsToFlip;
+
+                Console.WriteLine(ans);
+
+                //var a = Convert.ToString(tokens[0], 2);
+                //var b = Convert.ToString(tokens[1], 2);
+
+                //while (a.Length != b.Length)
+                //{
+                //    if (a.Length < b.Length)
+                //    { a = "0" + a; }
+                //    else
+                //    {
+                //      b = "0" + b;
+                //    } 
+                //}
+
+                //var ans = String.Join("",a.Select((value, index) => a[index] == b[index] && a[index] == '1' ? '0' : a[index]));
+
+                //int output = Convert.ToInt32(ans, 2);
+
+                //Console.WriteLine(output);
+            }
+        }
+    }
+}
---
+++ Tasks ( 51-100)/Task_2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace Task_2
+{
+    class Program
+    {
+        //        Least Significant Bit
+        //Time limit: 1220 ms
+        //Memory limit: 130.5 MB
+
+        //Template
+        //You should implement a function that takes as argument a positive integer xx and returns its least significant bit.More precisely you should return another integer whose binary representation contains a single bit set to 11, and that bit is the least significant one that is also set to 11 in xx.
+        static void Main(string[] args)
+        {
+            int n = Int32.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int x = Int32.Parse(Console.ReadLine());
+
+                int noLSB = x & (x - 1);
+
+                int ans = x ^ noLSB;
+
+                Console.WriteLine(ans);
+
+                //string x = Convert.ToString(Int32.Parse(Console.ReadLine()), 2);
+
+                //var ans = x.Length - x.LastIndexOf('1') - 1;
+
+                //Console.WriteLine(1 << ans);
+            }
+        }
+    }
+}

2f5b1586329e626c7f4608f030884ea40e928ec8
vladtf <vendeta0263@gmail.com>
2020-02-27 20:41:37 +0200

    Solved 2 more tasks ( counting and Look and Say )

---
+++ Tasks ( 51-100)/Task_3/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+
+namespace Task_3
+{
+    //    Digit Holes
+    //Time limit: 1220 ms
+    //Memory limit: 258.5 MB
+
+    //When writing digits, some of them are considered to have holes: 00, 66 and 99 have one hole, while 88 has two holes.The other digits don't have any holes.
+
+    //Given two integers AA and BB, find a value in the interval[A, B][A, B] that has the maximum total number of holes in its digits.If the solution is not unique, print the smallest one.
+    class Program
+    {
+        private static int GetNrOfHoles(int x)
+        {
+            int count = 0;
+            if (x == 0)
+            {
+                return 1;
+            }
+            while (x != 0)
+            {
+                int lastD = x % 10;
+                if (lastD == 6 || lastD == 0 || lastD == 9)
+                {
+                    count++;
+                }
+                if (lastD == 8)
+                {
+                    count += 2;
+                }
+
+                x /= 10;
+            }
+            return count;
+        }
+        static void Main(string[] args)
+        {
+            var tokens = Console.ReadLine().Split(' ').Select(x => Int32.Parse(x)).ToList();
+
+            int a = tokens[0];
+            int b = tokens[1];
+
+            int maxHoles = GetNrOfHoles(a);
+            int ans = a;
+            for (int i = a; i <= b; i++)
+            {
+                if (GetNrOfHoles(i) > maxHoles)
+                {
+                    ans = i;
+                    maxHoles = GetNrOfHoles(i);
+                }
+            }
+
+            Console.WriteLine(ans);
+        }
+    }
+}
---
+++ Tasks ( 51-100)/Task_4/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace Task_4
+{
+    class Program
+    {
+        private static string TransformString(string initial)
+        {
+            //string output = String.Join("",initial.GroupBy(x => x).Select(x=>x.First()));
+
+            StringBuilder output = new StringBuilder();
+
+            int index = 0;
+            while (index < initial.Length)
+            {
+                int count = 1;
+                while (index < initial.Length && initial[index] == initial[index+1])
+                {
+                    index++;
+                    count++;
+                }
+
+                output.Append(count.ToString()+initial[index]);
+
+                index++;
+            }
+
+            return output.ToString();
+        }
+        static void Main(string[] args)
+        {
+            int n = Int32.Parse(Console.ReadLine());
+
+            string ans = "1";
+
+            for (int i = 0; i < n; i++)
+            {
+                ans = TransformString(ans);
+            }
+
+            Console.WriteLine(ans);
+         }
+    }
+}

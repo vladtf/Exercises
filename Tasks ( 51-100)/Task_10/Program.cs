2ce286dca360c90cd77c8091887cef623fea6f6a
vladtf <vendeta0263@gmail.com>
2020-03-03 22:45:14 +0200

    Solved 2 more tasks  ( arrays and subarrays )

---
+++ Tasks ( 51-100)/Task_10/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace Task_10
+{
+    internal class Program
+    {
+        //        Alternant Array
+        //Time limit: 1220 ms
+        //Memory limit: 258.5 MB
+
+        //You have a binary array VV of size 2 * N2∗N composed of NN values of 00 and NN values of 11. You can swap any two elements.What is the minimum number of swaps to make the array alternant? A binary array is /alternant/ if no two consecutive elements are equal.
+        private static void Main(string[] args)
+        {
+            int n = Int32.Parse(Console.ReadLine());
+
+            var tokens = Console.ReadLine().Split(" ").Select(x => Int32.Parse(x)).ToList();
+
+            int count1 = 0;
+            int count2 = 0;
+            for (int i = 0; i < tokens.Count(); i += 2)
+            {
+                if (tokens[i] != 0)
+                {
+                    count1++;
+                }
+                else
+                {
+                    count2++;
+                }
+            }
+
+            Console.WriteLine(Math.Min(count1, count2));
+        }
+    }
+}
---
+++ Tasks ( 51-100)/Task_9/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Linq;
+
+namespace Task_9
+{
+    internal class Program
+    {
+        //        Monotone Subarray
+        //Time limit: 1220 ms
+        //Memory limit: 258.5 MB
+
+        //You are given an array AA of size NN.Consider the following definitions:
+
+        //An array is non-increasing if for every ii, A_i \geq A_{ i + 1}
+        //        A
+        //​i
+        //​​ ≥A
+        //​i+1
+        //​​ .
+        //Similarly, it is non-decreasing if for every ii, A_i \leq A_ { i+1}
+        //        A
+        //​i
+        //​​ ≤A
+        //​i+1
+        //​​ .
+        //An array is monotone if it is non-increasing or non-decreasing.
+        //Find the longest monotone subarray.
+        private static void Main(string[] args)
+        {
+            int n = Int32.Parse(Console.ReadLine());
+
+            var tokens = Console.ReadLine().Split(" ").Select(x => Int32.Parse(x)).ToList();
+
+            int l = 1;
+            int lmax = l;
+            for (int i = 1; i < n; i++)
+            {
+                if (tokens[i - 1] <= tokens[i])
+                {
+                    l++;
+
+                    if (l > lmax)
+                    {
+                        lmax = l;
+                    }
+                }
+                else
+                {
+                    l = 1;
+                }
+            }
+
+            l = 1;
+            for (int i = 1; i < n; i++)
+            {
+                if (tokens[i - 1] >= tokens[i])
+                {
+                    l++;
+
+                    if (l > lmax)
+                    {
+                        lmax = l;
+                    }
+                }
+                else
+                {
+                    l = 1;
+                }
+            }
+
+            Console.WriteLine(lmax);
+        }
+    }
+}

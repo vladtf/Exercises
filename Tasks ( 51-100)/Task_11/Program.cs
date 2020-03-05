cc8fde5ebee7f3f3905fcb318aa44086466dbfcc
vladtf <vendeta0263@gmail.com>
2020-03-05 22:50:06 +0200

    Solved 2 more tasks

---
+++ Tasks ( 51-100)/Task_11/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace Task_11
+{
+    internal class Program
+    {
+        //        Max Wave Array
+        //Time limit: 1220 ms
+        //Memory limit: 130.5 MB
+
+        //An array AA is called wave array if A_1 > A_2<A_3> A_4< ...A
+        //Given an array AA, permute its elements such that it becomes a wave array. If the solution is not unique, find the largest lexicographical one.
+        private static void Main(string[] args)
+        {
+            int n = Int32.Parse(Console.ReadLine());
+
+            var tokens = Console.ReadLine().Split(" ").Select(x => Int32.Parse(x)).OrderByDescending(x => x).ToList();
+
+            for (int i = 1; i < tokens.Count() - 1; i += 2)
+            {
+                int aux = tokens[i];
+                tokens[i] = tokens[i + 1];
+                tokens[i + 1] = aux;
+            }
+
+            tokens.ForEach(x => Console.Write(x + " "));
+        }
+    }
+}
---
+++ Tasks ( 51-100)/Task_12/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Task_12
+{
+    internal class Program
+    {
+        //        Bounded Distinct
+        //Time limit: 1220 ms
+        //Memory limit: 130.5 MB
+
+        //You are given an array of NN integers.Find the number of subarrays having a length between LL and RR that contain only distinct elements.
+        private static void Main(string[] args)
+        {
+            var tokens = ReadIntArray();
+
+            int n = tokens[0];
+            int l = tokens[1];
+            int r = tokens[2];
+
+            tokens = ReadIntArray();
+
+            var temp = new List<int>();
+            temp.Add(tokens[0]);
+
+            int ans = 0;
+
+            for (int i = 1; i < tokens.Count(); i++)
+            {
+                if (temp.Count >= l && temp.Count <= r)
+                    ans++;
+
+                while (temp.Contains(tokens[i]))
+                {
+                    temp.RemoveAt(0);
+                }
+                temp.Add(tokens[i]);
+
+                if (temp.Count >= l)
+                {
+                    ans += temp.Count - l + 1;
+                }
+            }
+            Console.WriteLine(ans);
+        }
+
+        private static List<int> ReadIntArray()
+        {
+            return Console.ReadLine().Split(" ").Select(x => Int32.Parse(x)).ToList();
+        }
+    }
+}
